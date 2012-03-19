60e1fac52bf9ed6869adcf0d7059d762f2bc5250
Joni <joni@feidm4.(none)>
2012-03-19 16:42:54 +0200

    Added default rails skeleton

---
+++ moco/rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moco::Application.config.secret_token = 'b6df0ecc89b9877ccd4d8fcd923b121a78e3192a57e33cb88cc1ff5bd254456f2ca361ab6372022acaee38267147769f1da93868d347b2142f54010a9cf7b96e'
