49d9d1d9bfd73c64bd478ed432e76ff069b7ac9d
Joni R <joni.rajanen@gmail.com>
2012-03-26 18:30:43 +0300

    added initial example jmpress presentation

--- moco/rails/app/assets/javascripts/moco.js
+++ moco/rails/app/assets/javascripts/moco.js
@@ -3,21 +3,20 @@ var Moco = new function() {
     this.slide = 0; 
 
     this.init = function(id, poll) {
-	this.id = id;
-	setInterval(Moco.update, poll);
+      this.id = id;
+      setInterval(Moco.update, poll);
     };
 
     this.dispose = function() {
-	window.clearInterval(this.interval);
+      window.clearInterval(this.interval);
     };
 
     this.update = function() {
-        //alert('updated: ' + Moco.id);
-	$.get('http://localhost:3000/presentations/1/status', function(data) {
-		if(data.currentSlide > Moco.slide) {
-			$("#jmpress").jmpress('next');
-			Moco.slide++;
-		}	
-	});
+      $.get('http://localhost:3000/presentations/'+ Moco.id +'/status', function(data) {
+	      if(data.currentSlide > Moco.slide) {
+		      $("#jmpress").jmpress('next');
+		      Moco.slide++;
+	      }	
+      });
     };
 }
