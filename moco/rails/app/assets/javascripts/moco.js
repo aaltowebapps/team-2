becd87ebc8c8bc203292b12aa3d7d5efc9ce5b57
Joni R <joni.rajanen@gmail.com>
2012-03-26 19:16:47 +0300

    simple presentation controlling

--- moco/rails/app/assets/javascripts/moco.js
+++ moco/rails/app/assets/javascripts/moco.js
@@ -1,22 +1,34 @@
 var Moco = new function() {
-    this.id = -1;
-    this.slide = 0; 
+	this.id = -1;
+	this.slide = 0; 
 
-    this.init = function(id, poll) {
-      this.id = id;
-      setInterval(Moco.update, poll);
-    };
+	this.init = function(id, poll) {
+		this.id = id;
+		setInterval(Moco.update, poll);
+	};
 
-    this.dispose = function() {
-      window.clearInterval(this.interval);
-    };
+	this.dispose = function() {
+		window.clearInterval(this.interval);
+	};
 
-    this.update = function() {
-      $.get('http://localhost:3000/presentations/'+ Moco.id +'/status', function(data) {
-	      if(data.currentSlide > Moco.slide) {
-		      $("#jmpress").jmpress('next');
-		      Moco.slide++;
-	      }	
-      });
-    };
+	this.update = function() {
+		$.get('http://localhost:3000/presentations/'+ Moco.id +'/status', function(data) {
+				var diff = Moco.slide - data.currentSlide;
+				if(diff != 0) { // transition is required
+					if(Math.abs(diff) > 1) { // jump
+						var selector = '#slide-'+data.currentSlide;
+						$("#jmpress").jmpress('goTo', selector);
+						Moco.slide = data.currentSlide;
+					}
+					else if(diff < 0) { // forward
+						$("#jmpress").jmpress('next');
+						Moco.slide++;
+					}
+					else if(diff > 0) { // backward
+						$("#jmpress").jmpress('prev');
+						Moco.slide--;
+					}
+				}
+		});
+	};
 }
